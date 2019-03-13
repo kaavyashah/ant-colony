d1ceb5fa0fe0bd26377582c130a92d718b7bad70
bridget cheng <bridget.c@berkeley.edu>
2019-03-13 11:07:22 -0700

    Rough version edge selection

--- AntColonizationOptimization.py
+++ AntColonizationOptimization.py
@@ -1,3 +1,5 @@
+import random
+
 class AntColonyOptimization:
 
     def __init__(self, graph):
@@ -21,12 +23,53 @@ class AntColonyOptimization:
         for ant in self.num_ants:
             ants.append(Ant(self.num_nodes, self.starting_node))
 
+        self.trail_levels = [[0 for i in range(self.num_nodes)] for i in range(self.num_nodes)]
+
+    def get_trail_level(self, n1, n2):
+        """
+            Given two nodes, return the trail level of the edge between
+            them.
+        """
+        return self.trail_levels[n1][n2]
+
+    def set_trail_level(self, n1, n2, new_level):
+        """
+            Given two nodes, set the trail level of the edge between
+            them to new_level.
+        """
+        self.trail_levels[n1][n2] = new_level
+
+    def gen_next_node(self, probabilities, total):
+        """
+            Given the probabilities for all possible edges and the sum
+            of all probabilities, generate the next node to be traversed.
+        """
+        rand_num = random.uniform(0, 1) * denom
+        for i in range(len(probabilities)):
+            p = probabilities[i]
+            if rand_num - p <= 0:
+                return i
+
     def select_edge(self, ant):
         '''
             Given ant at specific node, calculates probabilities for
             each edge and randomly selects the next edge to traverse.
         '''
-        return None
+        curr_node = ant.last_visited().i
+        numerators = []
+
+        for n in range(self.num_nodes): #all nodes are neighbors for our purpose
+            curr_numer = 0
+            if n != curr_node: #guaranteed to not stay at same node
+                attractiveness = 1 / self.graph.get_distance(curr_node, n)
+                trail_level = self.get_trail_level(curr_node, n)
+                curr_numer = attractiveness * trail_level
+            numerators.append(curr_numer)
+
+        total_probl = sum(numerators)
+        probls = [n / total_probl for n in numerators]
+        next_node = self.gen_next_node(probls, total_probl)
+        ant.visit_node(next_node)
 
     def pheromone_update(self):
         ''' At the end of each iteration, update trails for all paths the ants took.'''
